6f9d64a4349e5a28ccc88fbeeefc2159f1f8ebdc
Merlim3 <joacijunior309@gmail.com>
2025-04-14 02:15:35 +0000

    Desafio_cartas_trunfo_P1

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -1,22 +1,46 @@
 #include <stdio.h>
+ int main (){
 
-// Desafio Super Trunfo - Países
-// Tema 1 - Cadastro das Cartas
-// Este código inicial serve como base para o desenvolvimento do sistema de cadastro de cartas de cidades.
-// Siga os comentários para implementar cada parte do desafio.
-//Teste larissa
+         printf("\nCartas super trunfo, aperte enter para continuar!\n");
+         getchar ();
 
-int main() {
-    // Sugestão: Defina variáveis separadas para cada atributo da cidade.
-    // Exemplos de atributos: código da cidade, nome, população, área, PIB, número de pontos turísticos.
-    
-    // Cadastro das Cartas:
-    // Sugestão: Utilize a função scanf para capturar as entradas do usuário para cada atributo.
-    // Solicite ao usuário que insira as informações de cada cidade, como o código, nome, população, área, etc.
-    
-    // Exibição dos Dados das Cartas:
-    // Sugestão: Utilize a função printf para exibir as informações das cartas cadastradas de forma clara e organizada.
-    // Exiba os valores inseridos para cada atributo da cidade, um por linha.
+         int  numero_de_pontos_turisticos=20;
+         float Area_em_km2 = 1392.0;
+         char Estado [20] = "Piaui";
+         char cidade [20]= "Teresina";
+         char codigo_carta [5] ="A01"; 
+         float PIB = 2300000000.0;
+         int Numero_populacional = 866300 ;  
 
-    return 0;
+       printf  ("\nCartas Trunfo :D\n");
+        printf ("\nEstado: %s \n", Estado);
+        printf ("Codigo da carta: %s \n", codigo_carta);
+        printf ("Nome da cidade: %s \n", cidade);
+        printf ("População: %i \n", Numero_populacional);
+        printf ("Area em Km2: %.2f\n" , Area_em_km2);  // Corrigido
+        printf ("PIB: %.2f\n", PIB);  // Corrigido
+        printf ("Numeros de pontos turisticos: %i \n",numero_de_pontos_turisticos);
+
+
+printf("\nClique enter de novo para ver outra carta!\n");
+  getchar ();
+
+int  numero_de_pontos_turisticos_2 =20;
+float Area_em_km2_2 = 3275838.0 ;
+char Estado_2 [20] = "Ceara";
+char cidade_2 [20]= "Quixeramobim";
+char codigo_carta_2 [5] ="A02"; 
+ float PIB_2 = 1405315389.0;
+ int Numero_populacional_2 = 85797;
+ 
+       printf ("\nCartas Trunfo :D\n");
+        printf ("\nEstado: %s \n", Estado_2);
+        printf ("Codigo da carta: %s \n", codigo_carta_2);
+        printf ("Nome da cidade: %s \n", cidade_2);
+        printf ("População: %i \n",  Numero_populacional_2);
+        printf ("Area em Km2: %.2f\n" , Area_em_km2_2);  // Corrigido
+        printf ("PIB: %.2f\n", PIB_2);  // Corrigido
+        printf ("Numeros de pontos turisticos: %i \n",numero_de_pontos_turisticos_2);
+
+return 0;
 }
